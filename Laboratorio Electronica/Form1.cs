6b791b63e6a9ed94ed6900dfde8b725d5662b215
thewolf003 <thewolf0310@outlook.com>
2022-09-19 23:24:46 -0500

    Metodos de conexion, evento seleccion de tuplas

--- Laboratorio Electronica/Form1.cs
+++ Laboratorio Electronica/Form1.cs
@@ -13,21 +13,66 @@ namespace Laboratorio_Electronica
 {
     public partial class Form1 : Form
     {
-        SqlConnection conexion = new SqlConnection("Server=DESKTOP-O0MDQRH\\SQLEXPRESS;" + "Database=Laboratorio;" + "Integrated Security=true;");
+        private int id;
+        private SqlConnection conexion = new SqlConnection("Server=DESKTOP-O0MDQRH\\SQLEXPRESS;" + "Database=Laboratorio;" + "Integrated Security=true;");
         public Form1()
         {
             InitializeComponent();
         }
-
-        private void empleadoToolStripMenuItem_Click(object sender, EventArgs e)
+        private void muestraVista()
         {
+            string query = String.Concat("SELECT * FROM Persona.Empleado");
 
+            SqlCommand cmd = new SqlCommand(query, conexion);
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+            DataTable registros = new DataTable();
+
+            adapter.Fill(registros);
+
+            dgridVista.DataSource = null;
+
+            dgridVista.DataSource = registros;
         }
 
-        private void colaboradorToolStripMenuItem_Click(object sender, EventArgs e)
+        private int conectaBD()
         {
-       
+            try
+            {
+                conexion.Open();
+                muestraVista();
+                conexion.Close();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                conexion.Close();
+                MessageBox.Show("Error de conexion: " + ex.Message);
+                return 1;
+            }
         }
+
+        private int insertarRegistro()
+        {
+            try
+            {
+                conexion.Open();
+                string consulta = "INSERT INTO Persona.Empleado() VALUES ()";
+                SqlCommand cmd = new SqlCommand(consulta, conexion);
+                cmd.ExecuteNonQuery();
+                conexion.Close();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                conexion.Close();
+                MessageBox.Show("Error de conexion: " + ex.Message);
+                return 1;
+            }
+        }
+
+
 
         private void tabPage1_Click(object sender, EventArgs e)
         {
@@ -36,17 +81,105 @@ namespace Laboratorio_Electronica
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            //Mostrar ocultar paneles
+        }
+                
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgridVista.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgridVista.MultiSelect = false;
+            dgridVista.EditMode = DataGridViewEditMode.EditProgrammatically;
+            btnModificar.Enabled = false;
+            btnEliminar.Enabled = false;
+            conectaBD();
+            dgridVista.ClearSelection();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btnAlta_Click(object sender, EventArgs e)
         {
+            insertarRegistro();
+            conectaBD();
+            //nombre.Clear(); //limpiar textbox
+            dgridVista.ClearSelection();
+        }
+        private int cargarDatos()
+        {
+            int selectedRowCount = dgridVista.Rows.GetRowCount(DataGridViewElementStates.Selected);
+            if (selectedRowCount > 0)
+            {
+                DataGridViewRow row = dgridVista.SelectedRows[0];
+                var id = row.Cells[0].Value;
+                var nombrePiloto = row.Cells[1];
+                var cultureInfo = new System.Globalization.CultureInfo("de-DE");
+                var fechaNacimiento = row.Cells[2].Value.ToString();
+                nombre.Text = nombrePiloto.Value.ToString();
+                fechaNacimiento = fechaNacimiento.Substring(0, fechaNacimiento.IndexOf(" "));
+                var dateTime = DateTime.ParseExact(fechaNacimiento, "MM/dd/yyyy", cultureInfo);
+                //fechaNac.Value = dateTime;
 
+                return int.Parse(id.ToString());
+            }
+            return -1;
         }
 
-        private void label9_Click(object sender, EventArgs e)
+        private void dgridVista_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
         {
+            id = cargarDatos(); //obtener id de tupla seleccionada
+            btnModificar.Enabled = true;
+            btnEliminar.Enabled = true;
+        }
 
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            if (id != -1)
+            {
+                try
+                {
+                    conexion.Open();
+                    string consulta = "UPDATE Persona.Empleado SET  WHERE idPiloto=" + id + ";";
+                    SqlCommand cmd = new SqlCommand(consulta, conexion);
+                    cmd.ExecuteNonQuery();
+                    conexion.Close();
+                }
+                catch (Exception ex)
+                {
+                    conexion.Close();
+                    MessageBox.Show("Error de conexion: " + ex.Message);
+                }
+            }
+            id = -1;
+            btnModificar.Enabled = false;
+            btnEliminar.Enabled = false;
+            conectaBD();
+            dgridVista.ClearSelection();
+            nombre.Clear();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (id != -1)
+            {
+                try
+                {
+                    conexion.Open();
+                    string consulta = "DELETE FROM Persona.Empleado WHERE idPiloto=" + id + ";";
+                    SqlCommand cmd = new SqlCommand(consulta, conexion);
+                    cmd.ExecuteNonQuery();
+                    conexion.Close();
+                }
+                catch (Exception ex)
+                {
+                    conexion.Close();
+                    MessageBox.Show("Error de conexion: " + ex.Message);
+                }
+            }
+            id = -1;
+            btnEliminar.Enabled = false;
+            btnModificar.Enabled = false;
+            conectaBD();
+            dgridVista.ClearSelection();
+            nombre.Clear();
         }
     }
 }
